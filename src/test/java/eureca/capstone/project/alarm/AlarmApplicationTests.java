a23b1741f1ce31c01b4384b07613f6c2a3bceac5
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-11 09:06:32 +0900

    first commit

---
+++ src/test/java/eureca/capstone/project/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package eureca.capstone.project.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
